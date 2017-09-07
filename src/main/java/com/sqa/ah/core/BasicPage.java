9f05a08a4d728e6c1a944c804dc945a668c8a77d
Anne Hesemann <anne.hesemann@gmail.com>
2017-09-06 22:07:10 -0700

    Create Core, BasicTest, and BasicPage classes

---
+++ src/main/java/com/sqa/ah/core/BasicPage.java
@@ -0,0 +1,26 @@
+/**
+ * File Name: BasicPage.java<br>
+ * Hesemann, Anne<br>
+ * Created: Sep 6, 2017
+ */
+package com.sqa.ah.core;
+
+import org.openqa.selenium.*;
+
+/**
+ * BasicPage
+ *
+ * @author Hesemann, Anne
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class BasicPage extends Core {
+
+	/**
+	 * @param baseurl
+	 * @param driver
+	 */
+	public BasicPage(String baseurl, WebDriver driver) {
+		super(baseurl, driver);
+	}
+}
